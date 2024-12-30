9bc8b16ed66c66f7e9014ad58aca9326d7feb6bf
Pearlie <bugrapearls@gmail.com>
2024-12-30 22:35:03 +0300

    Implemented the detour to 'modify' the dropchances.

--- PetEffects/ShadowMimic.cs
+++ PetEffects/ShadowMimic.cs
@@ -1,12 +1,32 @@
 ﻿using PetsOverhaul.Items;
 using PetsOverhaul.Systems;
+using System;
+using System.Collections.Generic;
 using Terraria;
+using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace PetsOverhaul.PetEffects
 {
+    public class ShadowMimicEquippedCondition : IItemDropRuleCondition
+    {
+        public bool CanDrop(DropAttemptInfo info)
+        {
+            return info.player.miscEquips[0].type == ItemID.OrnateShadowKey;
+        }
+
+        public bool CanShowItemDropInUI()
+        {
+            return true;
+        }
+
+        public string GetConditionDescription()
+        {
+            return "hello";
+        }
+    }
     public sealed class ShadowMimic : PetEffect
     {
         public override int PetItemID => ItemID.OrnateShadowKey;
@@ -17,11 +37,25 @@ namespace PetsOverhaul.PetEffects
         public int bossItem = 5;
         public int bagCoin = 10;
         public int bagItem = 5;
-        private int chanceToRollItem = 0;
+        public int chanceToRollItem = 30; //Numerator will be multiplied with this, Denominator will be multiplied by 100, effectively 30% of original chance
         public override void Load()
         {
             PetsOverhaul.OnPickupActions += PreOnPickup;
+            On_ItemDropResolver.ResolveRule += ShadowMimicExtraDrop;
         }
+
+        private static ItemDropAttemptResult ShadowMimicExtraDrop(On_ItemDropResolver.orig_ResolveRule orig, ItemDropResolver self, IItemDropRule rule, DropAttemptInfo info)
+        {
+            if (info.player.TryGetModPlayer(out ShadowMimic mimic) && mimic.PetIsEquipped() && rule is CommonDrop drop)
+            {
+                if (Math.Max(drop.chanceNumerator, 1) / Math.Max(drop.chanceDenominator, 1) <= 0.1)
+                {
+                    drop.chanceNumerator *= 5; //This currently maxes the droprates? Look into it!
+                }
+            }
+            return orig(self,rule,info);
+        }
+
         public static void PreOnPickup(Item item, Player player)
         {
             GlobalPet PickerPet = player.GetModPlayer<GlobalPet>();
@@ -48,6 +82,24 @@ namespace PetsOverhaul.PetEffects
             }
         }
     }
+    //public sealed class ShadowMimicLoot : GlobalNPC
+    //{
+    //    public override void OnKill(NPC npc)
+    //    {
+    //        DropAttemptInfo info = new();
+    //        On_ItemDropResolver.resolve
+    //    }
+    //    public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+    //    {
+    //        foreach (var rule in npcLoot.Get())
+    //        {
+    //            if (rule is CommonDrop drop && Math.Max(drop.chanceNumerator, 1) / Math.Max(drop.chanceDenominator, 1) < 0.2)
+    //            {
+    //                drop.OnFailedRoll(new LeadingConditionRule(new ShadowMimicEquippedCondition()).OnSuccess(ItemDropRule.Common(drop.itemId, 2)));
+    //            }
+    //        }
+    //    }
+    //}
     public sealed class OrnateShadowKey : PetTooltip
     {
         public override PetEffect PetsEffect => shadowMimic;
