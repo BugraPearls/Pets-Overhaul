61860588e5478c648c65fdd577a8530cbc93785e
Pearlie <bugrapearls@gmail.com>
2025-02-01 19:22:07 +0300

    Removed old Recipes, added new Methods for Recipes.

--- Systems/PetRecipes.cs
+++ Systems/PetRecipes.cs
@@ -1,71 +1,45 @@
 ﻿using PetsOverhaul.Items;
+using PetsOverhaul.Tiles;
+using System;
 using Terraria;
+using Terraria.GameContent.Achievements;
+using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
 namespace PetsOverhaul.Systems
 {
     public class PetRecipes : ModSystem
     {
+        /// <summary>
+        /// Obsolete, will be removed.
+        /// </summary>
         public static void MasterPetCraft(int result, int itemToPairWithMasteryShard)
         {
             Recipe.Create(result)
             .AddIngredient(ModContent.ItemType<MasteryShard>())
             .AddIngredient(itemToPairWithMasteryShard)
+            .AddTile(ModContent.TileType<PetForge>())
             .Register();
         }
+        /// <summary>
+        /// Pet Recipes all have Pet Food in their recipes, and crafted on Pet Forge.
+        /// </summary>
+        /// <param name="recipe">Recipe that will get registered, to be added Pet Food and to be crafted on a Pet Forge.</param>
+        /// <param name="petFoodAmount"></param>
+        public static void PetRecipe(Recipe recipe, int petFoodAmount = 1)
+        {
+            recipe.AddIngredient(ModContent.ItemType<PetFood>(), Math.Max(petFoodAmount,1))
+                .AddTile(ModContent.TileType<PetForge>())
+                .Register();
+        }
+        /// <summary>
+        /// Creates a Pet Recipe that also has a Mastery Shard as one of its ingredients.
+        /// </summary>
+        public static void MasterModePetRecipe(Recipe recipe, int petFoodAmount = 1, int masteryShardAmount = 1) => PetRecipe(recipe.AddIngredient<MasteryShard>(Math.Max(masteryShardAmount,1)), petFoodAmount);
+
         public override void AddRecipes()
         {
-            MasterPetCraft(ItemID.KingSlimePetItem, ItemID.KingSlimeTrophy);
-            MasterPetCraft(ItemID.EyeOfCthulhuPetItem, ItemID.EyeofCthulhuTrophy);
-            MasterPetCraft(ItemID.EaterOfWorldsPetItem, ItemID.EaterofWorldsTrophy);
-            MasterPetCraft(ItemID.BrainOfCthulhuPetItem, ItemID.BrainofCthulhuTrophy);
-            MasterPetCraft(ItemID.QueenBeePetItem, ItemID.QueenBeeTrophy);
-            MasterPetCraft(ItemID.SkeletronPetItem, ItemID.SkeletronTrophy);
-            MasterPetCraft(ItemID.DeerclopsPetItem, ItemID.DeerclopsTrophy);
-            MasterPetCraft(ItemID.QueenSlimePetItem, ItemID.QueenSlimeTrophy);
-            MasterPetCraft(ItemID.DestroyerPetItem, ItemID.DestroyerTrophy);
-            MasterPetCraft(ItemID.TwinsPetItem, ItemID.RetinazerTrophy);
-            MasterPetCraft(ItemID.TwinsPetItem, ItemID.SpazmatismTrophy);
-            MasterPetCraft(ItemID.SkeletronPrimePetItem, ItemID.SkeletronPrimeTrophy);
-            MasterPetCraft(ItemID.PlanteraPetItem, ItemID.PlanteraTrophy);
-            MasterPetCraft(ItemID.DukeFishronPetItem, ItemID.DukeFishronTrophy);
-            MasterPetCraft(ItemID.LunaticCultistPetItem, ItemID.AncientCultistTrophy);
-            MasterPetCraft(ItemID.MoonLordPetItem, ItemID.MoonLordTrophy);
-            MasterPetCraft(ItemID.DD2OgrePetItem, ItemID.BossTrophyOgre);
-            MasterPetCraft(ItemID.DD2BetsyPetItem, ItemID.BossTrophyBetsy);
-            MasterPetCraft(ItemID.MartianPetItem, ItemID.MartianSaucerTrophy);
-            MasterPetCraft(ItemID.EverscreamPetItem, ItemID.EverscreamTrophy);
-            MasterPetCraft(ItemID.IceQueenPetItem, ItemID.IceQueenTrophy);
-
-            MasterPetCraft(ModContent.ItemType<LihzahrdWrench>(), ItemID.GolemTrophy);
-            MasterPetCraft(ModContent.ItemType<PrismaticOptic>(), ItemID.FairyQueenTrophy);
-            MasterPetCraft(ModContent.ItemType<PumpkingsHead>(), ItemID.PumpkingTrophy);
-
-            Recipe.Create(ItemID.ShadowOrb)
-                .AddIngredient(ItemID.ShadowScale, 12)
-                .AddTile(TileID.Anvils)
-                .Register();
-            Recipe.Create(ItemID.CrimsonHeart)
-                .AddIngredient(ItemID.TissueSample, 12)
-                .AddTile(TileID.Anvils)
-                .Register();
-
-            Recipe.Create(ItemID.GolemPetItem)
-                .AddIngredient(ItemID.LunarTabletFragment, 10)
-                .AddCondition(Condition.PlayerCarriesItem(ModContent.ItemType<LihzahrdWrench>()))
-                .AddTile(TileID.MythrilAnvil)
-                .Register();
-            Recipe.Create(ItemID.FairyQueenPetItem)
-                .AddIngredient(ItemID.SoulofFlight, 15)
-                .AddCondition(Condition.PlayerCarriesItem(ModContent.ItemType<PrismaticOptic>()))
-                .AddTile(TileID.MythrilAnvil)
-                .Register();
-            Recipe.Create(ItemID.PumpkingPetItem)
-                .AddIngredient(ItemID.Pumpkin, 75)
-                .AddIngredient(ItemID.SpookyWood, 25)
-                .AddCondition(Condition.PlayerCarriesItem(ModContent.ItemType<PumpkingsHead>()))
-                .AddTile(TileID.MythrilAnvil)
-                .Register();
+            MasterModePetRecipe(Recipe.Create(ItemID.MartianPetItem).AddIngredient(ItemID.MartianConduitPlating, 500).AddIngredient(ItemID.Hoverboard), 1500); //Example: This is 500 Martian Conduit Plating, 1 Hoverboard, 75~ Gold Coins (1500 Pet Food) and 1 Mastery Shard.
         }
     }
 }
