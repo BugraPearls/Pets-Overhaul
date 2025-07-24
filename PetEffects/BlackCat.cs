34699f22337361c38a41ee9d5ae6098ebaa4349a
BugraPearls <bugrapearls@gmail.com>
2025-07-24 20:16:32 +0300

    Custom effects for these guys

--- PetEffects/BlackCat.cs
+++ PetEffects/BlackCat.cs
@@ -1,4 +1,5 @@
-﻿using PetsOverhaul.Systems;
+﻿using PetsOverhaul.Config;
+using PetsOverhaul.Systems;
 using System;
 using Terraria;
 using Terraria.Audio;
@@ -6,6 +7,7 @@ using Terraria.DataStructures;
 using Terraria.GameInput;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace PetsOverhaul.PetEffects
 {
@@ -23,7 +25,62 @@ namespace PetsOverhaul.PetEffects
         public int moonlightLowest = -10;
         public int moonlightHighest = 20;
         public float currentMoonLuck = 0;
-        public override int PetAbilityCooldown => moonlightCd;
+        public override int PetAbilityCooldown => CustomEffectActive ? lunarVeilCooldown : moonlightCd;
+
+        //Custom effect
+        public override int PetStackCurrent => lunarVeilDuration > 0 ? lunarVeilDuration : lunarVeilPostDuration;
+        public override string PetStackText => PetTextsColors.LocVal("CustomPetEffects.UnluckyYarnStack");
+        public override string PetStackSpecial => CustomEffectActive ? PetTextsColors.SecondsOutOfText(PetStackCurrent,0) : string.Empty;
+        public override bool CustomEffectIsContributor => false;
+        public override bool HasCustomEffect => true;
+        public override PetClasses CustomPrimaryClass => PetClasses.Defensive;
+        public int lunarVeilDuration = 0;
+        public int lunarVeilDurationMax = 600;
+        public int lunarVeilCooldown = 7200;
+        public int lunarVeilPostDuration = 0;
+        public int lunarVeilPostDurationMax = 480;
+        public float lunarVeilMs = 0.06f;
+        public int lunarVeilManaRegen = 10;
+        public override void SaveData(TagCompound tag)
+        {
+            tag.Add("CustomActive", CustomEffectActive);
+        }
+        public override void LoadData(TagCompound tag)
+        {
+            if (tag.TryGet("CustomActive", out bool custom))
+            {
+                CustomEffectActive = custom;
+            }
+        }
+        public override void ExtraPreUpdateNoCheck()
+        {
+            if (PetIsEquippedForCustom())
+            {
+                if (lunarVeilDuration > 0)
+                {
+                    lunarVeilDuration--;
+                }
+                if (lunarVeilPostDuration > 0)
+                {
+                    lunarVeilPostDuration--;
+                }
+            }
+        }
+        public override void PostUpdateMiscEffects()
+        {
+            if (PetIsEquippedForCustom() && lunarVeilPostDuration > 0)
+            {
+                Player.moveSpeed += lunarVeilMs;
+                Player.manaRegenBonus += lunarVeilManaRegen;
+            }
+        }
+        public override void UpdateBadLifeRegen()
+        {
+            if (PetIsEquippedForCustom() && lunarVeilPostDuration > 0 && Player.lifeRegen > 0)
+            {
+                Player.lifeRegen = 0;
+            }
+        }
         public override void ModifyLuck(ref float luck)
         {
             if (PetIsEquipped())
@@ -47,37 +104,68 @@ namespace PetsOverhaul.PetEffects
                 luck += currentMoonLuck + luckFlat;
             }
         }
-        public override void ProcessTriggers(TriggersSet triggersSet)
+        public override void ExtraProcessTriggers(TriggersSet triggersSet)
         {
-            if (Main.dayTime == false && Pet.AbilityPressCheck() && PetIsEquipped())
+            if (Main.dayTime == false && Pet.AbilityPressCheck())
             {
-                SoundEngine.PlaySound(SoundID.Item29 with { PitchRange = (-1f, -0.8f) }, Player.Center);
-                int moonlightRoll = Main.rand.Next(moonlightLowest, moonlightHighest + 1);
-                moonlightRoll = GlobalPet.Randomizer((int)(moonlightRoll * (Player.luck + 1) * 100));
-                if (moonlightRoll == 0)
+                if (PetIsEquipped())
                 {
-                    moonlightRoll = Main.rand.NextBool() ? -1 : 1;
-                }
-                if (moonlightRoll < 0)
-                {
-                    moonlightRoll *= -1;
-                    string reason = Main.rand.Next(5) switch
+                    if (ModContent.GetInstance<PetPersonalization>().AbilitySoundEnabled)
                     {
-                        0 => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath1"),
-                        1 => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath2"),
-                        2 => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath3"),
-                        3 => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath4"),
-                        4 => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath5"),
-                        _ => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath1"),
-                    };
-                    Player.Hurt(PlayerDeathReason.ByCustomReason(reason.Replace("<name>", Player.name)), moonlightRoll, 0, dodgeable: false, knockback: 0, scalingArmorPenetration: 1f);
+                        SoundEngine.PlaySound(SoundID.Item29 with { PitchRange = (-1f, -0.8f) }, Player.Center);
+                    }
+                    int moonlightRoll = Main.rand.Next(moonlightLowest, moonlightHighest + 1);
+                    moonlightRoll = GlobalPet.Randomizer((int)(moonlightRoll * (Player.luck + 1) * 100));
+                    if (moonlightRoll == 0)
+                    {
+                        moonlightRoll = Main.rand.NextBool() ? -1 : 1;
+                    }
+                    if (moonlightRoll < 0)
+                    {
+                        moonlightRoll *= -1;
+                        string reason = Main.rand.Next(5) switch
+                        {
+                            0 => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath1"),
+                            1 => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath2"),
+                            2 => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath3"),
+                            3 => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath4"),
+                            4 => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath5"),
+                            _ => PetTextsColors.LocVal("PetItemTooltips.BlackCatDeath1"),
+                        };
+                        Player.Hurt(PlayerDeathReason.ByCustomReason(reason.Replace("<name>", Player.name)), moonlightRoll, 0, dodgeable: false, knockback: 0, scalingArmorPenetration: 1f);
+                    }
+                    else
+                    {
+                        Pet.PetRecovery(moonlightRoll, 1f, isLifesteal: false);
+                    }
+                    Pet.timer = Pet.timerMax;
                 }
-                else
+                else if (PetIsEquippedForCustom())
                 {
-                    Pet.PetRecovery(moonlightRoll, 1f, isLifesteal: false);
+                    if (ModContent.GetInstance<PetPersonalization>().AbilitySoundEnabled)
+                    {
+                        SoundEngine.PlaySound(SoundID.Item29 with { PitchRange = (-1f, -0.8f) }, Player.Center);
+                    }
+                    lunarVeilDuration = lunarVeilDurationMax;
+                    Pet.timer = Pet.timerMax;
                 }
-                Pet.timer = Pet.timerMax;
             }
+        }
+        public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genDust, ref PlayerDeathReason damageSource)
+        {
+            if (PetIsEquippedForCustom() && lunarVeilDuration > 0)
+            {
+                if (ModContent.GetInstance<PetPersonalization>().AbilitySoundEnabled)
+                {
+                    SoundEngine.PlaySound(SoundID.Item29 with { PitchRange = (0.5f, 1f) }, Player.Center);
+                }
+                Player.statLife = 1;
+                lunarVeilPostDuration = lunarVeilPostDurationMax;
+                lunarVeilDuration = 0;
+                Player.SetImmuneTimeForAllTypes(30); //gives half a second IFrames, this isn't mentioned in tooltip though
+                return false;
+            }
+            return base.PreKill(damage, hitDirection, pvp, ref playSound, ref genDust, ref damageSource);
         }
     }
 
@@ -105,5 +193,13 @@ namespace PetsOverhaul.PetEffects
                 .Replace("<moonLuck>", Math.Round(blackCat.currentMoonLuck, 2).ToString())
                 .Replace("<playerLuck>", Math.Round(blackCat.Player.luck, 2).ToString());
         public override string SimpleTooltip => PetTextsColors.LocVal("SimpleTooltips.UnluckyYarn").Replace("<keybind>", PetTextsColors.KeybindText(PetKeybinds.UsePetAbility));
+        public override string CustomTooltip => PetTextsColors.LocVal("CustomPetEffects.UnluckyYarn")
+            .Replace("<keybind>", PetTextsColors.KeybindText(PetKeybinds.UsePetAbility))
+            .Replace("<seconds>", Math.Round(blackCat.lunarVeilDurationMax / 60f, 2).ToString())
+            .Replace("<cooldown>", Math.Round(blackCat.lunarVeilCooldown / 60f, 2).ToString())
+            .Replace("<postDuration>", Math.Round(blackCat.lunarVeilPostDurationMax / 60f, 2).ToString())
+            .Replace("<ms>", Math.Round(blackCat.lunarVeilMs * 100, 2).ToString())
+            .Replace("<manaRegen>", blackCat.lunarVeilManaRegen.ToString());
+        public override string CustomSimpleTooltip => PetTextsColors.LocVal("SimpleCustomPetEffects.UnluckyYarn").Replace("<keybind>", PetTextsColors.KeybindText(PetKeybinds.UsePetAbility));
     }
 }
--- PetEffects/PropellerGato.cs
+++ PetEffects/PropellerGato.cs
@@ -1,8 +1,11 @@
 ﻿using PetsOverhaul.Projectiles;
 using PetsOverhaul.Systems;
+using System;
 using Terraria;
+using Terraria.GameInput;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace PetsOverhaul.PetEffects
 {
@@ -13,11 +16,33 @@ namespace PetsOverhaul.PetEffects
         public int turretIncrease = 1;
 
         public override PetClasses PetClassPrimary => PetClasses.Summoner;
+        public override bool CustomEffectIsContributor => true;
+        public override bool HasCustomEffect => true;
+        public override PetClasses CustomPrimaryClass => PetClasses.Mobility;
+
+        //Custom effect field
+        public int wingTime = 90;
+        public override void SaveData(TagCompound tag)
+        {
+            tag.Add("CustomActive", CustomEffectActive);
+        }
+        public override void LoadData(TagCompound tag)
+        {
+            if (tag.TryGet("CustomActive", out bool custom))
+            {
+                CustomEffectActive = custom;
+            }
+        }
         public override void PostUpdateMiscEffects()
         {
             if (PetIsEquipped())
             {
                 Player.maxTurrets++;
+            }
+            else if (PetIsEquippedForCustom())
+            {
+                Player.wingTimeMax += wingTime;
+                Player.statDefense += 2;
             }
         }
         public override void ModifyHitNPCWithProj(Projectile proj, NPC target, ref NPC.HitModifiers modifiers)
@@ -39,6 +64,7 @@ namespace PetsOverhaul.PetEffects
                 }
             }
         }
+
     }
     public sealed class DD2PetGato : PetTooltip
     {
@@ -57,5 +83,8 @@ namespace PetsOverhaul.PetEffects
                         .Replace("<crit>", propellerGato.bonusCritChance.ToString())
                         .Replace("<maxSentry>", propellerGato.turretIncrease.ToString());
         public override string SimpleTooltip => PetTextsColors.LocVal("SimpleTooltips.DD2PetGato");
+        public override string CustomTooltip => PetTextsColors.LocVal("CustomPetEffects.DD2PetGato")
+            .Replace("<wingTime>", Math.Round(propellerGato.wingTime / 60f, 2).ToString());
+        public override string CustomSimpleTooltip => PetTextsColors.LocVal("SimpleCustomPetEffects.DD2PetGato");
     }
 }
