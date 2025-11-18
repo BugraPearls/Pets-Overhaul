ce4d62f032868f029f8995af7364d71199a71d73
BugraPearls <bugrapearls@gmail.com>
2025-11-18 08:33:44 +0300

    Added Forging Pets and Mastery Craft (Will do names & description etc. later)

---
+++ Achievements/ForgingPets.cs
@@ -0,0 +1,23 @@
+﻿using Terraria.Achievements;
+using Terraria.ModLoader;
+using Terraria.GameContent.Achievements;
+using PetsOverhaul.Systems;
+using System.Linq;
+using System.Collections.Generic;
+using Terraria.ID;
+
+namespace PetsOverhaul.Achievements
+{
+    public class ForgingPets : ModAchievement
+    {
+        public ItemCraftCondition PetCrafted { get; private set; }
+        public override void SetStaticDefaults()
+        {
+            List<int> list = [.. PetIDs.LightPetNamesAndItems.Values];
+            list.AddRange(PetIDs.PetNamesAndItems.Values);
+            
+            Achievement.SetCategory(AchievementCategory.Collector);
+            PetCrafted = AddItemCraftCondition([.. list]);
+        }
+    }
+}
---
+++ Achievements/MasteryCraft.cs
@@ -0,0 +1,33 @@
+﻿using Terraria.Achievements;
+using Terraria.ModLoader;
+using Terraria.GameContent.Achievements;
+using PetsOverhaul.Systems;
+using System.Linq;
+using System.Collections.Generic;
+using Terraria.ID;
+
+namespace PetsOverhaul.Achievements
+{
+    public class MasteryCraft : ModAchievement
+    {
+        public CustomFlagCondition PetCrafted { get; private set; }
+        public override void Load()
+        {
+            AchievementsHelper.OnItemCraft += MasterShardCrafts;
+        }
+
+        private void MasterShardCrafts(short itemId, int count)
+        {
+            if (ContentSamples.ItemsByType[itemId].master && (PetIDs.PetNamesAndItems.ContainsValue(itemId) || PetIDs.LightPetNamesAndItems.ContainsValue(itemId)))
+            {
+                PetCrafted.Complete();
+            }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            Achievement.SetCategory(AchievementCategory.Collector);
+            PetCrafted = AddCondition();
+        }
+    }
+}
