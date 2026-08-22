3510868c87358204aa8bcffbe9ab3b6c8ed8d705
BugraPearls <bugrapearls@gmail.com>
2026-08-22 22:28:57 +0300

    Default Active Pet Slot positions now does not overlap with PVP buttons.

--- Config/PetPersonalization.cs
+++ Config/PetPersonalization.cs
@@ -64,7 +64,7 @@ namespace PetsOverhaul.Config
 
         [LabelKey("$Mods.PetsOverhaul.Config.ActivePetSlotPosLabel")]
         [TooltipKey("$Mods.PetsOverhaul.Config.ActivePetSlotPosTooltip")]
-        [DefaultValue(typeof(Vector2), "0.89, 0.439")]
+        [DefaultValue(typeof(Vector2), "0.87, 0.439")]
         [BackgroundColor(35, 120, 54, 190)]
         public Vector2 ActivePetSlotPos { get; set; }
 
