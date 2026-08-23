67d2908f78eca85cce70cbfaafe2b4fa02ec3ced
BugraPearls <bugrapearls@gmail.com>
2026-08-23 10:57:04 +0300

    System to check if a boss is alive (including non-boss = true bosses)

---
+++ Systems/PetOnWorldSystems.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace PetsOverhaul.Systems
+{
+    public class PetOnWorldSystems : ModSystem
+    {
+        public override void PostUpdateNPCs()
+        {
+            PetModPlayer.anyAliveBosses = false;
+            foreach (NPC npc in Main.ActiveNPCs)
+            {
+                if (npc.boss || PetIDs.NonBossTrueBosses.Contains(npc.type))
+                {
+                    PetModPlayer.anyAliveBosses = true;
+                }
+            }
+        }
+    }
+}
